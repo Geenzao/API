5e371acb42da561472c1eb8fc303351a5ce5c5de
Geenzao <quentinlorenzo04@gmail.com>
2024-03-20 17:25:27 +0100

    Les defis s'affichent maitenant avec seulement le titre et la description

--- routes/defisRoutes.js
+++ routes/defisRoutes.js
@@ -9,17 +9,26 @@ router.get('/random', async (req, res) => {
         const count = await Defi.countDocuments();
         const random = Math.floor(Math.random() * count);
         const defi = await Defi.findOne().skip(random);
-        res.json(defi);
+
+        res.status(200).json({
+            titre: defi.titre,
+            description: defi.description,
+        });
+
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
 });
 
+
 // Récupération de plusieurs défis aléatoires
 router.get('/multiple', async (req, res) => {
     try {
         const defis = await Defi.aggregate([{ $sample: { size: 100 } }]);
-        res.json(defis);
+        res.status(200).json({
+            titre: defis.titre,
+            description: defis.description,
+        });
     } catch (err) {
         res.status(500).json({ message: err.message });
     }
